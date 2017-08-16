1f2f61627564c54d4d201db943b271f48cead217
Vi Gi <vigneshnataraj@gmail.com>
2017-08-17 07:51:42 +1000

    set up server

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,24 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// kick off express server
+const server = express();
+
+// middleware
+server.use(bodyParser.json());
+
+// set up error response in json
+server.use((error, req, res, next) => {
+  const status = error.status || 500
+  res.status(status).json({
+    error: { message: error.message }
+  });
+}); 
+
+const port = process.env.PORT || 8000
+
+// Start local server
+server.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
